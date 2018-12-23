f205ca8c44f05f12ce20df0c646431fe35d4b6ea
TIMETOKNOW\Yakov.Kamenetsky <yakov.kamenetsky@timetoknow.co.il>
2018-12-23 16:55:26 +0200

    Identity scafolding

---
+++ FinalWebApp/MainApp/Areas/Identity/Data/UserData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainApp.Areas.Identity.Data
+{
+    public class UserData : IdentityUser
+    {
+        [PersonalData]
+        public string FirstName { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
+        [PersonalData]
+        public DateTime BirthDate { get; set; }
+
+    }
+}
---
+++ FinalWebApp/MainApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MainApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MainApp.Areas.Identity.IdentityHostingStartup))]
+namespace MainApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
