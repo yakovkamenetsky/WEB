4068bfa6dfce22aba4a7e975082292c3f1bf7867
nati <User@DESKTOP-EPSM398>
2018-12-30 13:41:29 +0200

    display features

---
+++ FinalWebApp/FinalWebApp/wwwroot/js/Features.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    var listOfFeatures = document.createElement("UL");
+    listOfFeatures.setAttribute("id", "listOfFeatures");
+    document.getElementById("featuresView").appendChild(listOfFeatures);
+
+    var featuresList = ["Alpha", "Beta", "Gama"];
+    for (i = 0; i < featuresList.length; i++) {
+
+        var feature = document.createElement("LI");
+
+        var featureFromList = document.createTextNode(featuresList[i]);
+
+        feature.appendChild(featureFromList);
+
+        document.getElementById("listOfFeatures").appendChild(feature);
+
+    }
+});
