0efdca171150c872c9ebe2aad85d3145affe9c33
Itzik Kahana <ikahana@allot.com>
2019-01-23 22:46:24 +0200

    ResultsView

---
+++ FinalWebApp/FinalWebApp/Controllers/SearchController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalWebApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        public async Task<IActionResult> Index(string place, DateTime checkin, DateTime checkout)
+        {
+
+			//var optionsBuilder = new DbContextOptions<MyContext>();
+
+			//var context = new MyContext(optionsBuilder);
+			//var res = await context.Hotel
+			//	.Where(x => x.Name.Contains(place) || x.City.Name.Contains(place))
+			//	.ToListAsync();
+
+			var results = new List<Hotel>
+			{
+				new Hotel()
+				{
+					Address = "hayarkon 55",
+					Name = "hilton",
+					Capacity = 10,
+					Price = 500,
+					Id = 1
+				}
+			};
+
+			//ViewBag["res"] = res;
+
+            return View("Results", results);
+        }
+    }
+}
--- FinalWebApp/FinalWebApp/Models/Hotel.cs
+++ FinalWebApp/FinalWebApp/Models/Hotel.cs
@@ -10,9 +10,11 @@ namespace FinalWebApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int CityId { get; set; }
-        public ICollection<Order> Orders { get; set; }
+		public City City { get; set; }
+		public ICollection<Order> Orders { get; set; }
         public string Address { get; set; }
         public int Capacity { get; set; }
+		public float Price { get; set; }
 	}
 
 }
