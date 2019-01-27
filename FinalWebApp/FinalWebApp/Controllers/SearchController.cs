68ac51ce483816e0fcd5d836d72859b6c69c1a50
Itzik Kahana <ikahana@allot.com>
2019-01-27 21:52:00 +0200

    Improving hotels view, using bootstrap.

--- FinalWebApp/FinalWebApp/Controllers/SearchController.cs
+++ FinalWebApp/FinalWebApp/Controllers/SearchController.cs
@@ -29,6 +29,24 @@ namespace FinalWebApp.Controllers
 					Capacity = 10,
 					Price = 500,
 					Id = 1
+				},
+
+				new Hotel()
+				{
+					Address = "London",
+					Name = "hilton",
+					Capacity = 20,
+					Price = 5000,
+					Id = 2
+				},
+
+				new Hotel()
+				{
+					Address = "Eilat",
+					Name = "hilton",
+					Capacity = 22,
+					Price = 540,
+					Id = 3
 				}
 			};
 
