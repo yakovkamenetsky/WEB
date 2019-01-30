51ea567e40ad2cba9c3cbf0c7911f39f46cca5fd
nati <User@DESKTOP-EPSM398>
2019-01-30 17:18:56 +0200

    adding price to initialize

--- FinalWebApp/FinalWebApp/Migrations/20190130151414_price.Designer.cs
+++ FinalWebApp/FinalWebApp/Migrations/20190130151414_price.Designer.cs
@@ -4,14 +4,16 @@ using FinalWebApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FinalWebApp.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20190130151414_price")]
+    partial class price
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
