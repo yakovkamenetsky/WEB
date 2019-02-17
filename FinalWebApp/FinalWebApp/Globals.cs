11d6679c54dd4dd53fb03444d2f627848cdb8ab9
nati <User@DESKTOP-EPSM398>
2019-02-17 22:43:01 +0200

    login and register views

---
+++ FinalWebApp/FinalWebApp/Globals.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace FinalWebApp
+{
+    static public class Globals
+    {
+        public const string ADMIN_SESSION_KEY = "isAdmin";
+        public const string USER_SESSION_KEY = "user";
+
+        static public string getConnectedUser(ISession session)
+        {
+            return session.GetString(USER_SESSION_KEY);
+        }
+
+        static public bool isAdminConnected(ISession session)
+        {
+            return session.GetString(ADMIN_SESSION_KEY) == "true";
+        }
+    }
+}
