8c20b510e70b9300fcd710d5c7e5e54db28f9532
nati <User@DESKTOP-EPSM398>
2019-02-23 20:33:19 +0200

    ui

--- FinalWebApp/FinalWebApp/wwwroot/js/site.js
+++ FinalWebApp/FinalWebApp/wwwroot/js/site.js
@@ -2,6 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/*
 $(function () {
     var body = $('body');
     var backgrounds = new Array(
@@ -24,21 +25,6 @@ $(function () {
     setTimeout(nextBackground, 5000);
     body.css('background', backgrounds[0]);  
     body.css('background-size', 'cover');
-});
-/*
-$(function () {
-
-    setTimeout(checkAdmin, 5000);
-
-    function checkAdmin() {
-        var editButton = document.getElementById("editButton");
-        if (sessionStorage.getItem("isUserAdmin") == "true") {
-            editButton.style.display = "block";
-        } else {
-            editButton.style.display = "none";
-        }
-        setTimeout(checkAdmin, 5000);
-    }
 });*/
 
 function showOrHideFeatures() {
